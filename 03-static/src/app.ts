f514bdc66387088df949fcd37af1c7f2c8e54757
rolldeep2 <lyw960820@naver.com>
2023-12-12 18:47:47 +0900

    Learn Static

---
+++ 03-static/src/app.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+
+const port = 8090;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  return res.send('Hello, NodeJs');
+});
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
+});
+
+// 에러 미들웨어 파라미터 4개 항상
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  return res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, () => {
+  console.log(`Listening and serving HTTP on : ${port}`);
+});
