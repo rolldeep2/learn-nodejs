9a031cb64668f5f15ff824e48e63be4c2eba5bad
rolldeep2 <lyw960820@naver.com>
2023-12-14 18:35:50 +0900

    Learn API!

---
+++ 04-api/src/app.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+
+const port = 8080;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  return res.send('Hello Express');
+});
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  return res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, () => {
+  console.log(`Listening and serving HTTP on :${port}`);
+});
