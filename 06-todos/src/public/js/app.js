4a58f396315eb91233d1ce90f2879c838a47170e
rolldeep2 <lyw960820@naver.com>
2023-12-28 19:33:05 +0900

    Todo API Update

--- 06-todos/src/public/js/app.js
+++ 06-todos/src/public/js/app.js
@@ -1,3 +1,6 @@
+const todoForm = document.querySelector('form');
+const todoFormInput = todoForm.querySelector('input');
+const todoFormButton = todoForm.querySelector('button');
 const deleteSelectionButton = document.querySelector('.selection');
 const deleteAllButton = document.querySelector('.all');
 const todoList = document.querySelector('ul');
@@ -35,7 +38,33 @@ const createTodo = (data) => {
 
   span.innerText = data.content;
 
+  span.addEventListener('click', (event) => {
+    todos = todos.map((todo) => {
+      return { ...todo, isUpdate: todo.id === data.id };
+    });
+
+    todoFormInput.value = event.target.innerText;
+    todoFormButton.innerText = 'Update';
+  });
+
   button.innerText = 'Delete';
+
+  // await 비동기쓸때, try catch 사용
+  button.addEventListener('click', async () => {
+    try {
+      await axios.delete(`/todos?ids=${data.id}`);
+
+      todos = todos.filter((todo) => todo.id !== data.id);
+
+      deleteSelectionButton.hidden = !todos.some((todo) => todo.isComplete);
+      deleteAllButton.hidden = !todos.length;
+
+      // 클로저
+      todoList.removeChild(li);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 
   li.appendChild(input);
   li.appendChild(span);
@@ -43,6 +72,89 @@ const createTodo = (data) => {
 
   todoList.appendChild(li);
 };
+
+todoForm.addEventListener('submit', async (event) => {
+  try {
+    event.preventDefault();
+
+    const selectedTodo = todos.find((todo) => todo.isUpdate);
+
+    const content = todoFormInput.value.trim();
+
+    if (!selectedTodo && content) {
+      const { data } = await axios.post('/todos', { content });
+
+      createTodo(data);
+
+      todoFormInput.value = '';
+    }
+
+    if (selectedTodo && content) {
+      await axios.patch(`/todos/${selectedTodo.id}/content`, { content });
+
+      const index = todos.findIndex((todo) => todo.id === selectedTodo.id);
+
+      todos[index].content = content;
+
+      todoFormInput.value = '';
+
+      todoFormButton.innerText = 'Create';
+
+      const span = todoList.childNodes[index].querySelector('span');
+
+      span.innerText = content;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+deleteSelectionButton.addEventListener('click', async () => {
+  try {
+    const ids = todos.reduce((result, todo) => {
+      return todo.isComplete ? [...result, todo.id] : result;
+    }, []);
+
+    await axios.delete(`/todos?ids=${ids.join(',')}`);
+
+    const indexes = todos.reduce((result, todo, index) => {
+      return ids.includes(todo.id) ? [index, ...result] : result;
+    }, []);
+
+    // indexes = [2,1,0]
+    indexes.forEach((index) => {
+      // todos = [{id:1, ...},{id:2, ...},{id:3, ...}]
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      todos.splice(index, 1);
+      // todos = [{id:1, ...},{id:2, ...}]
+
+      // childrens = [li, li, li...] index 유지를 위해 뒤에서부터 지움.
+      todoList.childNodes[index].remove();
+    });
+
+    deleteSelectionButton.hidden = !todos.some((todo) => todo.isComplete);
+
+    deleteAllButton.hidden = !todos.length;
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+deleteAllButton.addEventListener('click', async () => {
+  try {
+    await axios.delete('/todos');
+
+    todos = [];
+
+    deleteSelectionButton.hidden = true;
+
+    deleteAllButton.hidden = true;
+
+    todoList.innerHTML = '';
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 const app = async () => {
   try {
